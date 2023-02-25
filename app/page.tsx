004a666c2f720f86b72b06c34261af47f210d951
Michael Owuor <michael.owuor@gmail.com>
2023-02-25 11:09:21 +0300

    fix: add semantic html element in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-const Home = () => {
+export default function Home() {
 	return (
-		<>
+		<main>
 			<h1 className="text-xl uppercase">Welcome to The Home Page</h1>
 			<p className="text-lg">Your settings are well configured</p>
-		</>
+		</main>
 	);
-};
-
-export default Home;
+}
